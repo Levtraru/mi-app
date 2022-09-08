6096a8b50e484d05eae697013492310c536d2c52
Levtraru <lautarogt94@gmail.com>
2022-09-07 23:32:11 -0300

    clean code

--- src/components/VelcampLogo.js
+++ src/components/VelcampLogo.js
@@ -1,12 +1,7 @@
-import velCampLogo from '../images/velcamp-logo.png'
+import velCampLogo from "../images/velcamp-logo.png";
 
 const VelcampLogo = () => (
-    <div className="velcamp-logo-contenedor">
-            <img
-                className='velcamp-logo'
-                src={velCampLogo}
-                alt='Logo de velCamp'/>
-            </div>
+  <img className="velcamp-logo" src={velCampLogo} alt="Logo de velCamp" />
 );
 
-export default VelcampLogo
+export default VelcampLogo;
